b22bf585abbdb5c5b62a95b9f90ca2fd947c2ba8
zhangzhen <1094705507@qq.com>
2019-03-12 23:55:38 +0800

    修改参数

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -8,7 +8,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
 	return {
-		todoClick: age => {
+		todoClick: () => {
 			dispatch({
 				type: 'hello',
 				name: 'zhangzhen'
